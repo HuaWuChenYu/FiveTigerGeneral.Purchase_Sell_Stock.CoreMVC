534500490ef19bcf8575d0da6fc2ace96374ffc4
武晨宇 <2012837663@qq.com>
2021-01-13 19:18:20 +0800

    添加项目文件。

---
+++ Purchase_Sell_Stock.MVC/Controllers/ModelController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Purchase_Sell_Stock.MVC.Controllers
+{
+    public class ModelController : Controller
+    {
+        public IActionResult Model()
+        {
+            return View();
+        }
+    }
+}
---
+++ Purchase_Sell_Stock.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Purchase_Sell_Stock.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
