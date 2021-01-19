c6ad04a81babc8b0fb72e52c40263ac154932e09
lenovo <lenovo@QIAO>
2021-01-19 09:59:48 +0800

    1

--- Purchase_Sell_Stock.MVC/Controllers/StroageController.cs
+++ Purchase_Sell_Stock.MVC/Controllers/StroageController.cs
@@ -17,7 +17,7 @@ namespace Purchase_Sell_Stock.MVC.Controllers
             return View();
         }
         /// <summary>
-        /// 入库订单表显示
+        /// 出库订单表显示
         /// </summary>
         /// <returns></returns>
         public IActionResult OutboundorderCombineShow()
@@ -25,14 +25,36 @@ namespace Purchase_Sell_Stock.MVC.Controllers
             return View();
         }
         /// <summary>
-        /// 入库订单详情
+        /// 出库定订单详情
         /// </summary>
+        /// <param name="outboundorderId"></param>
         /// <returns></returns>
         public IActionResult OutboundorderCombinebackfillPage(int outboundorderId)
         {
             ViewBag.outboundorderId = outboundorderId;
             return View();
         }
+
+        /// <summary>
+        /// 按单出库
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PressOutboundorderCombineShow()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 按单出库订单页面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PressOutboundorderCombinebackfillPage(int outboundorderId)
+        {
+            ViewBag.outboundorderId = outboundorderId;
+            return View();
+        }
+
+
+
         /// <summary>
         /// 商品以及 商品库存
         /// </summary>
@@ -49,6 +71,47 @@ namespace Purchase_Sell_Stock.MVC.Controllers
         {
             return View();
         }
+
+
+        /// <summary>
+        /// 入库订单显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult IncomingorderShow()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 入库信息详情页
+        /// </summary>
+        /// <param name="incomingorderid"></param>
+        /// <returns></returns>
+        public IActionResult IncomingordermodityDetailPage(int incomingorderid)
+        {
+            ViewBag.incomingorderid = incomingorderid;
+            return View();
+        }
+        /// <summary>
+        /// 按单入库订单显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PressIncomingorderShow()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 按单入库信息详情页
+        /// </summary>
+        /// <param name="incomingorderid"></param>
+        /// <returns></returns>
+        public IActionResult PressIncomingordermodityDetailPage(int incomingorderid)
+        {
+            ViewBag.incomingorderid = incomingorderid;
+            return View();
+        }
+
+
+
 
     }
 }
