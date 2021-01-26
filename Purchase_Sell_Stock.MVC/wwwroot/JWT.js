37fa5badf65e3a653fba4d51181106e75f0b6132
武晨宇 <2012837663@qq.com>
2021-01-26 11:12:58 +0800

    123

---
+++ Purchase_Sell_Stock.MVC/wwwroot/JWT.js
@@ -0,0 +1,11 @@
+﻿$.ajaxSetup({ //发送请求前触发
+    beforeSend: function (xhr) { //可以设置自定义标头
+        var token = getCookie("token");
+        if (token == "" || token == null || token == undefined) {
+            alert('请先登录');
+            location.href = '/Login/Show';
+            return;
+        }
+        xhr.setRequestHeader('Authorization', "Bearer " + token);
+    }
+});
